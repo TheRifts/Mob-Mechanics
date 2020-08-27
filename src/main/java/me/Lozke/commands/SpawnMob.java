38c8f337eb04f9d1c2a83a95aeb9b51e69935309
Lozke <17802931+Lozke@users.noreply.github.com>
2020-08-26 20:35:30 -0500

    Setup spawn mob command

--- src/main/java/me/Lozke/commands/SpawnMob.java
+++ src/main/java/me/Lozke/commands/SpawnMob.java
@@ -2,15 +2,28 @@ package me.Lozke.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Syntax;
+import me.Lozke.MobMechanics;
+import me.Lozke.data.ModifiableEntity;
+import me.Lozke.data.Rarity;
+import me.Lozke.data.Tier;
+import me.Lozke.utils.Text;
 import org.bukkit.entity.Player;
 
 @CommandAlias("spawn")
 public class SpawnMob extends BaseCommand {
 
     @Default
-    public boolean onSpawnMob(Player player, String[] args) {
-        //new CustomMob(EntityType.valueOf(args[0]), args[1], Tier.values()[Integer.parseInt(args[2])], Rarity.values()[Integer.parseInt(args[3])], null, player.getLocation());
-        return true;
+    @Syntax("<mob id> <rarity>")
+    @CommandCompletion("@mob-ids")
+    public static void onSpawnMob(Player player, String mobID, @Default("T1") Tier tier, @Default("COMMON")  Rarity rarity) {
+        ModifiableEntity entity = MobMechanics.getInstance().getMobManager().getModifiableEntity(mobID);
+        if (entity == null) {
+            player.sendMessage(Text.colorize("&cInvalid Mob ID! Mob ID is case sensitive."));
+            return;
+        }
+        MobMechanics.getInstance().getMobManager().spawnMob(tier, rarity, entity, player.getLocation());
     }
 }
