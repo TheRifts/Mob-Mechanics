ca85980817c8360254cac23107ff64486fd5c38d
Lozke <17802931+Lozke@users.noreply.github.com>
2020-11-10 23:19:03 -0600

    Cancel existing task

--- src/main/java/me/Lozke/managers/SpawnerManager.java
+++ src/main/java/me/Lozke/managers/SpawnerManager.java
@@ -32,6 +32,9 @@ public class SpawnerManager {
     }
 
     public void startSpawnerTask() {
+        if (tickSpawnersTask != null && !tickSpawnersTask.isCancelled()) {
+            tickSpawnersTask.cancel();
+        }
         this.tickSpawnersTask = new TickSpawnersTask(this);
     }
 
