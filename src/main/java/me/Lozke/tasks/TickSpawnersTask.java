f388cee6a2cd97dbdc2a878903fd6d1991e63b70
Lozke <17802931+Lozke@users.noreply.github.com>
2020-11-10 23:13:57 -0600

    Null check RiftsMob + Removed setting mob stats

--- src/main/java/me/Lozke/tasks/TickSpawnersTask.java
+++ src/main/java/me/Lozke/tasks/TickSpawnersTask.java
@@ -4,7 +4,6 @@ import me.Lozke.AgorianRifts;
 import me.Lozke.MobMechanics;
 import me.Lozke.data.*;
 import me.Lozke.managers.SpawnerManager;
-import me.Lozke.utils.ItemFactory;
 import me.Lozke.utils.NumGenerator;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -94,20 +93,9 @@ public class TickSpawnersTask extends BukkitRunnable {
             @Override
             public void run() {
                 RiftsMob mob = MobMechanics.getInstance().getBaseEntityManager().spawnBaseEntity(spawner, location);
+                if (mob == null) return;
                 MobMechanics.getInstance().getMobManager().trackEntity(mob);
                 mob.setSpawner(spawner);
-
-                Tier tier = mob.getTier();
-                Rarity rarity = mob.getRarity();
-
-                int minHp = ItemFactory.getArmourHP(tier, rarity, ItemFactory.RangeType.LOW);
-                int maxHP = ItemFactory.getArmourHP(tier, rarity, ItemFactory.RangeType.HIGH);
-                mob.setBaseStat(RiftsStat.HP, NumGenerator.rollInclusive(minHp, maxHP));
-
-                mob.setBaseStat(RiftsStat.DMG_LO, ItemFactory.getDamage(tier, rarity, ItemFactory.RangeType.LOW));
-                mob.setBaseStat(RiftsStat.DMG_HI, ItemFactory.getDamage(tier, rarity, ItemFactory.RangeType.HIGH));
-
-                mob.updateStats();
             }
         }.runTask(MobMechanics.getInstance());
     }
